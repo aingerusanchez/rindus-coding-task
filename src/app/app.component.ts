6f16c263f7ebfcb08dc7925e67058d2eaa45ae5d
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-18 17:08:21 +0100

    feat: add header on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// Environment
+import { environment as env } from '@env/environment.development';
+// Services
+import { AuthService } from '@core/services/auth.service';
+// Components
+import { HeaderComponent } from '@shared/layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  #auth = inject(AuthService);
+
+  title: string = env.title;
+
+  onLogout = () => this.#auth.logout();
 }
