23bef144a088c2f63157d5f94ba9f95669592920
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-18 18:49:07 +0100

    feat: show selected role in header

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -4,16 +4,20 @@ import {
   EventEmitter,
   Input,
   Output,
+  inject,
 } from '@angular/core';
 // Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+// Services
+import { RoleService } from '@core/services/role.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule,MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +25,6 @@ import { MatIconModule } from '@angular/material/icon';
 export class HeaderComponent {
   @Input({ required: true }) title: string = '';
   @Output() logout = new EventEmitter<void>();
+
+  role = inject(RoleService).role;
 }
