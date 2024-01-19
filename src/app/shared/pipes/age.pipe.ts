772581ce2386df299741712c028cfa8b85475f3a
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-19 21:49:35 +0100

    feat: create age pipe

---
+++ src/app/shared/pipes/age.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'age',
+  standalone: true,
+})
+export class AgePipe implements PipeTransform {
+  transform(value: string /* , ...args: unknown[] */): string {
+    if (!value) return '';
+
+    const today = new Date();
+    const birthDate = new Date(value);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+    return age.toString();
+  }
+}
