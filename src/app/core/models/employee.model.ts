735a3372d8fd38defdaf7c64837c476c7c15b822
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-19 21:49:48 +0100

    docs: comment employee model

--- src/app/core/models/employee.model.ts
+++ src/app/core/models/employee.model.ts
@@ -2,11 +2,17 @@ export type Position = 'Senior' | 'Junior' | 'Other';
 
 export interface Employee {
   id: number;
+  /** URL of the employee's avatar */
   avatar: string;
   name: string;
   surname: string;
   email: string;
+  /** Birth date in MM/DD/YYYY format */
   birthDate: string;
   position: Position;
+  /**
+   * Alternative position (optional)
+   * Only filled when position is 'Other'
+   */
   altPos?: string;
 }
