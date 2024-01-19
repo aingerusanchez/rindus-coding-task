fd299533119a6de30332c6c964a71f663f18c66e
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-19 17:48:05 +0100

    feat: connect search-bar with employee-list page

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 // Angular Material
 import { MatListModule } from '@angular/material/list';
 // Components
 import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
+
+const SEARCH_MIN_CHARS = 3;
 
 @Component({
   selector: 'app-employee-list',
@@ -13,7 +15,17 @@ import { SearchBarComponent } from '@shared/components/search-bar/search-bar.com
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  onSearch(query: string) {
+  searchHint = signal('Enter at least 3 characters');
+
+  onSearchChange(query: string) {
+    // Search text must have at least ${SEARCH_MIN_CHARS} characters
+    if (query.trim().length < SEARCH_MIN_CHARS) {
+      this.searchHint.set(`Enter at least ${SEARCH_MIN_CHARS} characters`);
+      // Clean filtered data
+      return;
+    }
+
+    this.searchHint.set('');
     console.log('query:', query);
   }
 }
--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -2,66 +2,168 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
-  Input,
   Output,
+  input,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
 // Angular Material
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-
-const QUERY_MIN_LENGTH = 3;
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
     MatAutocompleteModule,
+    MatMenuModule,
   ],
-  templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <mat-form-field appearance="outline">
+      <mat-label for="search-input">{{ placeholder() }}</mat-label>
+      <mat-icon matPrefix>search</mat-icon>
+      <input
+        #searchInput
+        id="search-input"
+        matInput
+        type="search"
+        [placeholder]="placeholder()"
+        (input)="type.emit($any($event.target).value)"
+      />
+      <!-- @if (options().length) {
+      <button
+        #optionsButton
+        matSuffix
+        mat-button
+        class="options-menu"
+        [matMenuTriggerFor]="menu"
+        (click)="showOptions($any($event.target).value)"
+      >
+        {{ selectedOption() || 'Search by' }}
+      </button>
+      }  -->
+      @if (hint()) {
+      <mat-hint align="start"> {{ hint() }} </mat-hint>
+      }
+    </mat-form-field>
+
+    <!-- @if (options().length) {
+    <mat-menu #menu="matMenu">
+      @for (option of options(); track $index) {
+      <button mat-menu-item (click)="changeOption(option)">{{ option }}</button>
+      }
+    </mat-menu>
+    } -->
+  `,
+  styles: [
+    `
+      :host {
+        width: 100%;
+        position: relative;
+      }
+
+      mat-form-field {
+        min-width: 200px;
+        width: clamp(200px, 100%, 100%);
+        display: flex;
+
+        // Remove autofill background
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus,
+        input:-webkit-autofill:active {
+          transition: background-color 5000s;
+          background: transparent;
+        }
+
+        input,
+        label {
+          display: block;
+          // max-width: 90%;
+          text-overflow: ellipsis;
+          overflow: hidden;
+          white-space: nowrap;
+
+          &.mat-form-field-autofilled {
+            background: red;
+          }
+        }
+
+        // .options-menu {
+        //   padding-right: 35px;
+        //   // margin-right: 12px;
+        //   text-wrap: nowrap;
+
+        //   &::after {
+        //     border-left: 5px solid transparent;
+        //     border-right: 5px solid transparent;
+        //     border-top: 5px solid;
+        //     content: '';
+        //     position: absolute;
+        //     right: 16px;
+        //     top: 50%;
+        //     -webkit-transform: translateY(-50%);
+        //     transform: translateY(-50%);
+        //   }
+        // }
+
+        mat-hint {
+          margin: 6px 0 0 32px;
+          font-size: 0.8rem;
+        }
+      }
+
+      :host ::ng-deep .mat-form-field-underline {
+        background-color: white !important;
+      }
+      :host
+        ::ng-deep
+        .mat-focused
+        .mat-form-field-underline
+        .mat-form-field-ripple {
+        background-color: white !important;
+      }
+
+      ::ng-deep input.mat-input-element {
+        color: white;
+      }
+    `,
+  ],
 })
 export class SearchBarComponent {
-  @Input() placeholder: string = 'Search';
-  @Output() search: EventEmitter<string> = new EventEmitter<string>();
+  placeholder = input('Search');
+  hint = input('');
+  // options = input<string[]>([]);
+  @Output() type: EventEmitter<string> = new EventEmitter<string>();
 
-  searchForm: FormGroup;
-  submitted = false;
+  // selectedOption = signal<string>(this.options()?.[0]);
 
-  get query(): AbstractControl<string> | null {
-    return this.searchForm.get('username');
+  constructor() {
+    // Initialize selected option
+    /* effect(
+      () => {
+        if (!this.selectedOption() && this.options().length) {
+          this.selectedOption.set(this.options()[0]);
+        }
+      },
+      {
+        allowSignalWrites: true,
+      }
+    ); */
   }
 
-  constructor(private fb: FormBuilder) {
-    this.searchForm = this.fb.group({
-      query: ['', [Validators.minLength(QUERY_MIN_LENGTH)]],
-    });
-
-    // Avoid white spaces in the username input
-    this.query?.valueChanges.subscribe((value) =>
-      this.query?.setValue(value.trim(), { emitEvent: false })
-    );
+  /* showOptions(option: string) {
+    console.log(option);
   }
 
-  onSubmit(): void {
-    this.submitted = true;
-    if (this.searchForm.valid && this.query?.value) {
-      this.search.emit(this.query?.value);
-    }
-  }
+  changeOption(item: string) {
+    this.selectedOption.set(item);
+  } */
 }
