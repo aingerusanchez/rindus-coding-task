f9632e911eef422cb51076b71a7b8d36638b2d5e
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-20 14:52:45 +0100

    feat: create employee details page

---
+++ src/app/pages/employees/employee-details/employee-details.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-details.component.html',
+  styleUrl: './employee-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmployeeDetailsComponent {}
