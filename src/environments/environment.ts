aa3739c8bb57c76cbedbd1583e13ba8a93925494
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-01-21 17:24:03 +0100

    refactor: move constant values to environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,10 @@ export const environment = {
   production: true,
   slug: 'rindus_front_test',
   title: 'Employee Manager',
+  employeeForm: {
+    name: {
+      minChars: 3,
+      maxChars: 20,
+    },
+  },
 };
