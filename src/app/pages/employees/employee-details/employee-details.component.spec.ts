1c7aa6a451f0b81ffd3bca127a0b08b864b32a3c
Aingeru Sanchez <aingerusanchez72@gmail.com>
2024-02-07 13:23:00 +0100

    test: fix testing module config

--- src/app/pages/employees/employee-details/employee-details.component.spec.ts
+++ src/app/pages/employees/employee-details/employee-details.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeDetailsComponent } from './employee-details.component';
+import { EmployeeService } from '../employee.service';
+import { EmployeeApiService } from '@core/services/employee-api/employee-api.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('EmployeeDetailsComponent', () => {
   let component: EmployeeDetailsComponent;
@@ -8,10 +11,10 @@ describe('EmployeeDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EmployeeDetailsComponent]
-    })
-    .compileComponents();
-    
+      imports: [EmployeeDetailsComponent, BrowserAnimationsModule],
+      providers: [EmployeeService, EmployeeApiService],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EmployeeDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
